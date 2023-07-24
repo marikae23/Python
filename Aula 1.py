900ffd2a4b390aa28d892d628bf5d86017aae229
Maria Eduarda Eguchi <126472433+honnemari@users.noreply.github.com>
2023-07-24 16:43:31 -0300

    Create Aula 1.py

---
+++ Aula 1.py
@@ -0,0 +1,6 @@
+Construir um array em python de valores inteiros e imprimir o maior e o menor valor desse array: o array deve ter 10 elementos
+
+lista = [-5, 7, 43, 22, -20, 3, -28, 90, -160, 1, 20]
+
+print(max(lista))
+print(min(lista))

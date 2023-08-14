6bfe6efa175a786eba31c42d8e4f05652f816c9a
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-08-14 10:31:13 -0300

    Update Aula 4.py

--- Aula 4.py
+++ Aula 4.py
@@ -45,3 +45,13 @@ lenotanome()
 print("Meio do programa")
 avalianota()
 print("Fim do programa")
+
+___________________________________________________
+
+def imprime(l):
+    print(l):
+    print(id(l)):
+
+x = [1, 6, 4, 5, 8]
+imprime(x)
+print(id(x))

41f01bf8c09796b6066518abc975a2e5993425c7
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-08-14 09:29:19 -0300

    Update Aula 4.py

--- Aula 4.py
+++ Aula 4.py
@@ -4,22 +4,41 @@ nota = []
 nome = []
 
 def lenotanome ():
-    x = int(input("Digite a quantidade de alunos: "))
-    for i in range(x):
-        print("Aluno {}" .format(i))
-        n = float(input("Digite a nota do aluno: "))
-        m = input("Digite o nome do aluno: ")
-        nota.append(n)
-        nome.append(m)
+    try:
+        quantidade_alunos = int(input("Digite a quantidade de alunos: "))
+        for i in range(quantidade_alunos):
+            print("Aluno {}".format(i))
+            
+            while True:
+                try:
+                    nota = float(input("Digite a nota do aluno: "))
+                    if 0 <= nota <= 10:
+                        break
+                    else:
+                        print("Nota fora do intervalo válido (0-10).")
+                except ValueError:
+                    print("Digite uma nota válida (número).")
+
+            nome = input("Digite o nome do aluno: ")
+            
+            notas.append(nota)
+            nome.append(nome)
+            
+    except ValueError:
+        print("Digite uma quantidade válida de alunos.")
 
 def avalianota():
     for i in range(len(nome)):
-        if nota[i] >= 9.0 and nota[i] <= 10:
-            print("O alubno {} tem conceito A e nota {}" .format(nome[i], nota[i]))
-        if nota[i] <= 9.0 and nota[i] >= 8.0:
-            print("O aluno {} tem conceito B e nota {}" .format(nome[i], nota[i]))
-        if nota[i] >= 7.0 and nota[i] <= 8.0:
-            print("O aluno {} tem conceito C e nota {}" .format(nome[i], nota[i]))
+        if 9.0 <= nota[i] <= 10:
+            conceito = "A"
+        elif 8.0 <= nota[i] < 9.0:
+            conceito = "B"
+        elif 7.0 <= nota[i] < 8.0:
+            conceito = "C"
+        else:
+            conceito = "D"
+
+    print("O aluno {} tem conceito {} e nota {}".format(nome[i], conceito, nota[i]))
         
 print("O programa começa aqui")
 lenotanome()

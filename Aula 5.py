9d8076dad51b40c70dc16eeb7106ad9930de4080
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-08-21 09:10:53 -0300

    Update Aula 5.py

--- Aula 5.py
+++ Aula 5.py
@@ -1,3 +1,5 @@
+# Função que retorna o valor da potencia >_<
+
 import math
 
 def pot(x, y):
@@ -12,3 +14,47 @@ z = pot(x, y)
 print(z)
 
 print(math.sqrt(9))
+
+___________________________________________________
+
+import math
+
+while True:
+    try:
+        x = int(input("Digite a base: "))
+        y = int(input("Digite o exponte: "))
+        break
+
+    except:
+        print("Somente números")
+
+print(pow(x, y))
+print(math.sqrt(45))
+
+___________________________________________________
+
+import math
+
+def pot(x, y):
+    i = y 
+    z = 1
+    while i > 0:
+        i = i - 1
+        z = z * x
+    return(z)
+
+def entrada():
+    try:
+        x = int(input("Digite a base: "))
+        y = int(input("Digite o expoente: "))
+
+    except:
+        print("Somente números")
+    return(x, y)
+
+q, p = entrada()
+def imprime(w):
+    print("Resultado: ", w)
+
+z = pot(q, p)
+print(z)

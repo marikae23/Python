b1e52c8306eceaf1a606f7d191dcada3dbaa9301
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-08-21 08:31:41 -0300

    Update Aula 5.py

--- Aula 5.py
+++ Aula 5.py
@@ -1,4 +1,13 @@
+import math
+
+def pot(x, y):
+    i = y 
+    while i > 0:
+        i = i - 1
+
 x = int(input("Digite a base: "))
 y = int(input("Digite o expoente: "))
 
 print(pow(x, y))
+
+print(math, sqrt(9))

7039354da1737986235b8d44d10dc27fa4121804
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-08-28 10:23:09 -0300

    Update Aula 3.py

--- Aula 3.py
+++ Aula 3.py
@@ -21,3 +21,19 @@ l.insert(0, 11)
 print(l)  # A lista agora será [11, 9, 8, 7, 5, 4, 4, 3, 2]
 
 ___________________________________________________
+
+# dada a lista abaixo
+l = [5,4,3,2,8,7,4,9] #int
+# pede-se
+#1) imprima os 3 últimos elementos
+#2) ordene a em ordem crescente e decrescente
+#3) insira um elemento no início da l
+
+print(len(l))
+print(l[5: ]) # se eu quisesse imprimir os 3 primeiros seria print(l[ :3])
+l.sort()
+print(l)
+l.sort(reverse=True) # ou l.reverse()
+print(l)
+l.insert(0, 11)
+print(l)

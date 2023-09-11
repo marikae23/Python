3378a3acbac3efa37b794d36fb95ee05e5f010b5
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-09-11 10:31:12 -0300

    Update Revisão 1.py

--- Revisão 1.py
+++ Revisão 1.py
@@ -1,4 +1,4 @@
-# criar uma lista de inteiro e inserir n elementos
+#  criar uma lista de inteiro e inserir n elementos
 # inverter a lista de internos
 # retornar o produto dos elementos da lista criada
 # imprimir a lista
@@ -32,20 +32,27 @@ def menu():
         if int(op) == 1:
             lst = crialista()
         if int(op) == 2:
-            l = lst.reverse
+            lst = invert(lst)
         if int(op) == 3:
-            pass
+            print(produto_lista(lst))
+
         if int(op) == 4:
             imprimirlista(lst)
         if int(op) == 5:
             dic = criaDic()
         if int(op) == 6:
             imprimirDic(dic)
+        if int(op) == 7:
+            alterar(dic)
+        if int(op) == 8:
+            print(comp(lst))
+
+
 def crialista():
     l = []
-    x = int(input("quantos elementos pra lista? "))
+    x = int(input("quantos elementos pra lista?: "))
     for i in range(x):
-        elemento = int(input("digite o {} elemento da lista".format(i+1)))
+        elemento = int(input("digite o {} elemento da lista: ".format(i+1)))
         l.append(elemento)
     return l
 
@@ -56,10 +63,10 @@ def imprimirlista(lista):
 
 def criaDic():
     dic={}
-    x = int(input("elementos dicionario:"))
+    x = int(input("elementos dicionario: "))
     for i in range(x):
         nome = input("nome:")
-        nota = float(input("digite nota:"))
+        nota = float(input("digite nota: "))
         dic.get(nome)
         dic[nome] = nota
     return dic
@@ -69,6 +76,35 @@ def imprimirDic(dic):
     time.sleep(4)
 # programa principal
 
-def invert(lista)
+def invert(lista):
+    lista_invert = []
+    for i in range(len(lista), 0 , -1 ):
+        lista_invert.append(i)
+    return lista_invert
+
+def produto_lista(lst):
+    if len(lst) == 0:
+        return 1
+    else:
+        return lst[0] * produto_lista(lst[1:])
+def alterar(dic):
+    x = input("digite o nome do aluno: ")
+    dic[x] = int(input("digite a nova nota dele: "))
+
+def comp(lst):
+    iguais = []
+    lis = []
+    y = int(input("quantos elementos pra segunda lista?: "))
+    for i in range(y):
+        elemento = int(input("digite o {} elemento da lista: ".format(i + 1)))
+        lis.append(elemento)
+
+    for elemento_1 in lst:
+        for elemento_2 in lis:
+            if elemento_1 == elemento_2:
+                iguais.append(elemento_1)
+    return iguais
+
+
 
 menu()

4965a248477abcd803207e3579892922ce20e2f3
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-09-11 08:32:15 -0300

    Update Aula 7.py

--- Aula 7.py
+++ Aula 7.py
@@ -1,1 +1,27 @@
+#criar uma lista de inteiro e inserir n elementos
+#inverter a lista de internos
+#retornar o produto dos elementos da lista criada
+#imprimir a lista
+#criar um dicionário de n alunos com as respectivas notas
+#imprimir os alunos com suas notas
+#alterar a nota de um aluno do dicionário
+#teclar <sair> ou <SAIR> para abandonar o programa
 
+def menu():
+    while True:
+        print("criar uma lista")
+        op = input("digite a opção: ")
+        if op == 'sair'
+        break
+        
+def crialista():
+    print("função cria lista")
+    pass
+    #return(lista
+
+def inverterLista(lista):
+    print("A uniao B", a | b)
+    print("A interseção B", a & b)
+    print("diferença simétrica ", a^b)
+    print("B pertence a ", b in a)
+    print("2 pertence a ", 2 in a)

ed6a3675c577efd5282c3e1669581a9788bb4c5c
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-09-18 09:07:08 -0300

    Atualizar o Prova 1.py

--- Prova 1.py
+++ Prova 1.py
@@ -1,6 +1,6 @@
-# Carolina, 22301821
-# Matheus Musashi, 22300947
-# Maria Eduarda, 22302561
+# Carolina Maikuma Dias Lima , 22301821
+# Matheus Musashi Tanaka, 22300947
+# Maria Eduarda de Moura Eguchi, 22302561
 
 def menu()
     print("5. Função que receba uma lista desordenada, gerada por método rândomico, e retorne uma lista ordenada")

0badf09c413e2f9ff10f1687f68ee7c7b363a83c
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-09-18 08:45:56 -0300

    Update Revisão 1.py

--- Revisão 1.py
+++ Revisão 1.py
@@ -1,12 +1,3 @@
-#  criar uma lista de inteiro e inserir n elementos
-# inverter a lista de internos
-# retornar o produto dos elementos da lista criada
-# imprimir a lista
-# criar um dicionário de n alunos com as respectivas notas
-# imprimir os alunos com suas notas
-# alterar a nota de um aluno do dicionário
-# teclar <sair> ou <SAIR> para abandonar o programa
-
 import os
 import time
 

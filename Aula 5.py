a4e2e96d93871d0ba056341174a8071a6e422922
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-02 15:19:40 -0300

    Update Aula 5.py

--- Aula 5.py
+++ Aula 5.py
@@ -25,17 +25,20 @@ ___________________________________________________
 import math
 
 def pot(x, y):
-    i = y 
-    while i > 0:
-        i = i - 1
+    resultado = 1
+    for i in range(y):
+        resultado *= x
+    return resultado
 
 x = int(input("Digite a base: "))
 y = int(input("Digite o expoente: "))
 
 z = pot(x, y)
-print(z)
+print(f"{x} elevado a {y} é igual a {z}")
 
-print(math.sqrt(9))
+raiz_quadrada = math.sqrt(9)
+print(f"A raiz quadrada de 9 é igual a {raiz_quadrada}")
+
 
 ﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
@@ -44,66 +47,64 @@ import math
 while True:
     try:
         x = int(input("Digite a base: "))
-        y = int(input("Digite o exponte: "))
+        y = int(input("Digite o expoente: "))
         break
+    except ValueError:
+        print("Somente números são permitidos")
 
-    except:
-        print("Somente números")
+resultado_potencia = pow(x, y)
+print(f"{x} elevado a {y} é igual a {resultado_potencia}")
 
-print(pow(x, y))
-print(math.sqrt(45))
+raiz_quadrada = math.sqrt(45)
+print(f"A raiz quadrada de 45 é igual a {raiz_quadrada}")
 
 ﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
 import math
 
-def pot(x, y):
-    i = y 
-    z = 1
-    while i > 0:
-        i = i - 1
-        z = z * x
-    return(z)
+def potencia(x, y):
+    resultado = 1
+    for _ in range(y):
+        resultado *= x
+    return resultado
 
 def entrada():
-    try:
-        x = int(input("Digite a base: "))
-        y = int(input("Digite o expoente: "))
+    while True:
+        try:
+            x = int(input("Digite a base: "))
+            y = int(input("Digite o expoente: "))
+            return x, y
+        except ValueError:
+            print("Somente números inteiros são permitidos")
 
-    except:
-        print("Somente números")
-    return(x, y)
+base, expoente = entrada()
+resultado_potencia = potencia(base, expoente)
 
-q, p = entrada()
-def imprime(w):
-    print("Resultado: ", w)
-
-z = pot(q, p)
-print(z)
+imprime(resultado_potencia)
 
 ﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
-t = ("osmar", 123, 4, 5,[1, 2, 32, 4])
+t = ("osmar", 123, 4, 5, [1, 2, 32, 4])
 print(type(t))
 print(type(t[3]))
-print(l)
+
+l1 = t[4]  
+print(l1)
 print(t)
-print("O tamanho da tupla e", len(t))
-l1 - t[3]
+print("O tamanho da tupla é", len(t))
 
 for i in t:
     print(i)
 
 aluno = {
-    "01" : "Pedro",
-    "02" : "Antonio",
-    "10" : "Isabela",
-
-    "Lista" : [8.0, 9.0, 7.6, 8.0]
+    "01": "Pedro",
+    "02": "Antonio",
+    "10": "Isabela",
+    "Lista": [8.0, 9.0, 7.6, 8.0]
 }
 print(aluno.values())
 print(aluno.keys())
-print(aluno["01".aluno["Lista"][0]])
+print(aluno["01"], aluno["Lista"][0]) 
 print(aluno['Lista'])
 print(aluno["Lista"][0])
 

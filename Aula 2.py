611a2dc18a90589cd930eb1bf6641ac7bffe75da
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-02 15:34:58 -0300

    Update Aula 2.py

--- Aula 2.py
+++ Aula 2.py
@@ -2,29 +2,29 @@
 
 print("Hello World !")  # Imprime a mensagem "Hello World !"
 
-l = [1, 2, 57, 3, 9, 92]  # Cria uma lista chamada 'l' com os valores fornecidos
+i = [1, 2, 57, 3, 9, 92]  # Cria uma lista chamada 'i' com os valores fornecidos
 
-print(l)  # Imprime a lista completa: [1, 2, 57, 3, 9, 92]
+print(i)  # Imprime a lista completa: [1, 2, 57, 3, 9, 92]
 print(l[4])  # Imprime o quinto elemento da lista (índice 4): 9
-print(type(l))  # Imprime o tipo da variável 'l': <class 'list'>
-print(len(l))  # Imprime o comprimento (número de elementos) da lista: 6
-print(sum(l))  # Imprime a soma de todos os elementos da lista: 164
-print(max(l))  # Imprime o valor máximo da lista: 92
+print(type(i))  # Imprime o tipo da variável 'i': <class 'list'>
+print(len(i))  # Imprime o comprimento (número de elementos) da lista: 6
+print(sum(i))  # Imprime a soma de todos os elementos da lista: 164
+print(max(i))  # Imprime o valor máximo da lista: 92
 
-l.append(88)  # Adiciona o valor 88 ao final da lista 'l'
-print(l)  # Imprime a lista atualizada: [1, 2, 57, 3, 9, 92, 88]
+i.append(88)  # Adiciona o valor 88 ao final da lista 'l'
+print(i)  # Imprime a lista atualizada: [1, 2, 57, 3, 9, 92, 88]
 
 ___________________________________________________
 
 # Versão do professor:
 print("Hello World !")
 
-l = [1, 2, 57, 3, 9, 92]
-print(l)
-print(l[4])
-print(type(l))
-print(len(l))
-print(sum(l))
-print(max(l))
-l.append(88)
-print(l)
+i = [1, 2, 57, 3, 9, 92]
+print(i)
+print(i[4])
+print(type(i))
+print(len(i))
+print(sum(i))
+print(max(i))
+i.append(88)
+print(i)

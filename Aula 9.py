5de1f88eae671006ed0af84cac478b01a9859002
Maria Eduarda <126472433+marikae23@users.noreply.github.com>
2023-10-23 09:42:14 -0300

    Update Aula 9.py

--- Aula 9.py
+++ Aula 9.py
@@ -4,28 +4,52 @@ class Cliente:
         self.sobrenome = sobrenome
         self.cpf = cpf
 
-    def grava(self):
-        with open("cliente.txt", "a") as f:
-            f.write(f"Nome: {self.nome}\n")
-            f.write(f"Sobrenome: {self.sobrenome}\n")
-            f.write(f"CPF: {self.cpf}\n")
+    def grava(c):
+        f = open("cliente.txt", "a")
+        f.write(c.nome)
+        f.write(c.sobrenome)
+        f.write(c.cpf)
+        f.close()
 
-    def ler():
-        with open("cliente.txt", "r") as f:
-            print("Leitura de cliente")
-            content = f.read()
-            print(content)
-            
+    def ler(c):
+        f = open("cliente.txt", "r")
+        print("Leitura de cliente")
+        l = f.read()
+        print(l)
+        #for i n f:
+        #    print(i.)
+
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, cliente, saldo, limite):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
-        self.limite = limite
+        self.limite = limite 
 
-    def grava(self):
-        with open("conta.txt", "a") as f:
-            f.write(f"Número da Conta: {self.numero}\n")
-            f.write(f"Saldo: {self.saldo}\n")
-            f.write(f"Limite: {self.limite}\n")
-            f.write(f"Titular: {self.titular}\n")
+    def grava(c):
+        f = open("conta.txt", "a")
+        #cm = c.numero+str(c.saldo)+str(c.limite)+str(c.titular)
+        #f.write(cn)
+        f.write(c.numero)
+        f.write(str(c.saldo))
+        f.write(str(c.limite))
+        f.write(str(c.titular))
+        f.close()
+
+while True:
+    print ('''
+        1. Digite os dados
+        2. Ler arquivo e imprimir
+        3. Sair do programa
+        ''')
+
+    op = int(input("Digite a opção: "))
+    if op == 3:
+        break
+    if op == 1:
+        n = input("Digite nome do cliente: ")
+        s = input("Digite o sobrenome do cliente: ")
+        c = input("Digite o CPF do cliente: ")
+        num = input("Digite o número da conta: ")
+        sld = input("Digitw o saldo da conta: ")
+        lmy = input("Digite o limite da conta: ")
